c8742cfa4dff38047d6376328c74761783e8d260
Lavell Francis <Lavell.Francis@hotmail.com>
2025-08-15 22:23:40 +0100

    Added sample habitsor testing

--- models/Habit.js
+++ models/Habit.js
@@ -2,7 +2,10 @@ import mongoose from 'mongoose';
 
 const habitSchema = new mongoose.Schema({
   name: { type: String, required: true },
-  completedDates: [Date],
+  frequency: { type: String, required: true },
+  startDate: { type: Date, default: Date.now },
+  completedDates: { type: [Date], default: [] },
+  notes: { type: String, default: '' },
 });
 
 export default mongoose.model('Habit', habitSchema);
