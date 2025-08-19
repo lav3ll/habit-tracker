f37278fd42995d95be099f684b7f92f325cc2c44
Lavell Francis <Lavell.Francis@hotmail.com>
2025-08-19 23:22:59 +0100

    populated the habit pick list with test data

--- client/src/components/SelectHabit/SelectHabit.jsx
+++ client/src/components/SelectHabit/SelectHabit.jsx
@@ -1,16 +1,26 @@
 import React from 'react';
 import { Form } from 'react-bootstrap';
+import habitsData from '../utils/testHabitsData.json';
 
 const SelectHabit = () => {
   return (
-    <Form.Group controlId='habitSelect'>
-      <Form.Label>Choose a Habit</Form.Label>
-      <Form.Control as='select' defaultValue='0'>
-        <option value='0'>Select a Habit</option>
-        <option value='1'>Testing Value 1</option>
-        <option value='2'>Testing Value 2</option>
-      </Form.Control>
-    </Form.Group>
+    <>
+      <Form.Group controlId='habitSelect'>
+        <Form.Label>Choose a Habit</Form.Label>
+        <Form.Control as='select' defaultValue='0'>
+          {habitsData.map((habit) => (
+            <option key={habit.id} value={habit.id}>
+              {habit.name}
+            </option>
+          ))}
+        </Form.Control>
+      </Form.Group>
+
+      <Form>
+        <label htmlFor=''>Add Your Own Habit</label>
+        <Form.Control type='text' placeholder='Enter habit name' />
+      </Form>
+    </>
   );
 };
 
